4a09dff2314c9d69d835b5e5952abc82fb524e0d
kraoshin <killian.kerleau@outlook.fr>
2024-11-25 15:48:27 +0100

    create a function that produces output according to a format specifier

--- 1-print.c
+++ 1-print.c
@@ -1,23 +1,52 @@
 #include "main.h"
+
 /**
  *
  */
 
 int _printf(const char *format, ...)
 {
+	int i = 0, j = 0, k;
+
 	va_list args;
+
+	op_t specifier[] = {
+		{"c", print_char},
+		{NULL, NULL}
+	};
 
 	va_start(args, format);
 
-	while (*format)
+	if (!format)
+		exit(98);
+	while (format[i])
 	{
-		if (*format == '%')
+		if (format[i] == '%')
 		{
-			format ++;
-			if (*format == 'c')
-				charac(args);
+			k = i + 1;
+			while (specifier[j].op)
+			{
+				if (format[k] == *specifier[j].op)
+				{
+					specifier[j].f(args);
+					i += 2;
+				}
+				j++;
+			}
 		}
+		if (!specifier[j].op && format[k + 1] == '\0')
+			exit(0);
+		else if (!specifier[j].op && format[k + 1] != '\0')
+		{
+			_putchar(format[i]);
+			i += 2;
+		}
+		j = 0;
+		_putchar(format[i]);
+		i++;
 	}
+
 	va_end(args);
-	return (1);
+	return (0);
+
 }
