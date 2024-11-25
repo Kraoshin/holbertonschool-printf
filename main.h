14a086a6e19149472c8e8517a25f16cf2ef373a8
kraoshin <killian.kerleau@outlook.fr>
2024-11-25 15:44:56 +0100

    create a struct, add _putchar, print_char prototypes and different std library

--- main.h
+++ main.h
@@ -1,9 +1,21 @@
 #ifndef prnt_h
 #define prnt_h
 #include <unistd.h>
+#include <stdlib.h>
 #include <stdarg.h>
 
+int _putchar(char c);
 int _printf(const char *format, ...);
-int charac(char c);
+int print_char(va_list args);
+
+/**
+ *
+ */
+
+typedef struct op
+{
+	char *op;
+	int (*f)(va_list args);
+} op_t;
 
 #endif
