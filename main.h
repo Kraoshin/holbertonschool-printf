f2567346f201349b159fcfec5c7cc090a938cc33
kraoshin <killian.kerleau@outlook.fr>
2024-11-26 12:14:53 +0100

    add the library from the _putchar function

--- main.h
+++ main.h
@@ -1,12 +1,14 @@
-#ifndef prnt_h
-#define prnt_h
-#include <unistd.h>
+#ifndef main_h
+#define main_h
 #include <stdlib.h>
 #include <stdarg.h>
+#include <unistd.h>
 
 int _putchar(char c);
 int _printf(const char *format, ...);
 int print_char(va_list args);
+int print_string(va_list args);
+int specifier(const char charac, va_list args);
 
 /**
  *
