7ff17c8bfdfd7f1bf98848fcd4b0394d56340d54
root <root@Chacha.>
2024-11-26 16:48:49 +0100

    Create the function print_integer

--- function_print.c
+++ function_print.c
@@ -42,3 +42,41 @@ int print_percent(va_list args)
 
 	return (1);
 }
+
+/**
+ *
+ */
+
+int print_integer(va_list args)
+{
+	long int div, num = va_arg(args, int);
+	int power = 1, result = 0;
+
+	if (num < 0)
+	{
+		result += _puthcar('-');
+		num *= -1;
+	}
+
+	if (num < 10)
+		return (result += _putchar(num + '0'));
+
+	div = num;
+
+	while (div > 9)
+	{
+		power *= 10;
+		div /= 10;
+	}
+
+	while (power >= 1)
+	{
+		result += _putchar(((num / power) % 10) + '0');
+		power /= 10;
+	}
+
+	return (result);
+}
+
+
+
